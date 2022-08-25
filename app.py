3ce466527de0b8aa9f19ed4b11b756fdf72f17c6
shush <shushruthabhatt@gmail.com>
2022-08-25 16:55:07 -0700

    change fixed styles

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///satozbank"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
-app.config["SECRET_KEY"] = "your_key"
+app.config["SECRET_KEY"] = "abc123"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 
